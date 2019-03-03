e1ed48baf80c9b9e8f8b9e01157161e2724a62cf
Guillaume Rongier <guillaume.rongier@intersystems.com>
2019-03-03 11:01:22 +0100

    install.sh modfication

--- install.sh
+++ install.sh
@@ -16,19 +16,22 @@ fi
 instanceName=$1
 password=$2
 
-tClassImportDir=$DIR/install
+ClassImportDir=$DIR/install
+NameSpace="SAMPLES"
+CspPath="/csp/samples"
 
 irissession $instanceName -U USER <<EOF 
 SuperUser
 $password
-do \$system.OBJ.ImportDir("$tClassImportDir","*.cls","cubk",.errors,1)
+do \$system.OBJ.ImportDir("$ClassImportDir","*.cls","cubk",.errors,1)
 write "Complation de l'installer done"
 Set pVars("DirSamples")="$DIR/samples"
+Set pVars("NAMESPACE")="$NameSpace"
 Do ##class(App.Installer).setup(.pVars)
 zn "%SYS"
 
 set props("DeepSeeEnabled")=1
-set sc=##class(Security.Applications).Modify("/csp/samples", .props)
+set sc=##class(Security.Applications).Modify("$CspPath", .props)
 
 halt
 EOF
